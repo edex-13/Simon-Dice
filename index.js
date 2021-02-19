7528c706aa4fdcaa391af8a84515b3645d5ca8e7
edex13 <ederson12f@gmail.com>
2021-02-19 17:32:22 -0500

    empiezo a programar el juego

---
+++ index.js
@@ -0,0 +1,9 @@
+const BTN_JUEGO = document.getElementById('btnJuego');
+const BTN_COLOR_VIOLETA = document.getElementById('btn__colorVioleta');
+const BTN_COLOR_VERDE = document.getElementById('btn__colorVerde');
+const BTN_COLOR_CELESTE = document.getElementById('btn__colorCeleste');
+const BTN_COLOR_NARANJA = document.getElementById('btn__colorNaranja');
+
+BTN_JUEGO.addEventListener('click', () => {
+   console.log('Juego Iniciado');
+});
